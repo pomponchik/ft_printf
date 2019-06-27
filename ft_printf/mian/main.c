a5dfa9a220749796614792cc748e97be8bb83b17
Annara farring Halmon paege <ahalmon-@at-e6.21-school.ru>
2019-06-27 22:58:22 +0300

    in progress

--- ft_printf/mian/main.c
+++ ft_printf/mian/main.c
@@ -12,15 +12,6 @@
 #include <stdio.h>
 #include "../head.h"
 
-typedef struct			s_num
-{
-	char *number;
-	char *up;
-	int exp;
-}						t_num;
-
-
-
 char *integer_part(unsigned long int mantisa, unsigned int num_bits)
 {
 	char *listing;
@@ -44,134 +35,95 @@ char *integer_part(unsigned long int mantisa, unsigned int num_bits)
 	free(listing);
 	return (ft_strjoin_free_1(result, "."));
 }
-//
-// char *fdcv(t_list *lst)
-// {
-// 	char *five;
-// 	int ex;
-// 	t_num *pa;
-// 	char *temp;
-// 	int index = 0;
-// 	t_list *ls;
-// 	t_list *l;
-// 	char *result = ft_strdup("0");
-//
-// 	pa = lst->content;
-// 	ex = pa->exp;
-// 	temp = pa->number;
-// 	pa->up = ft_strdup("1");
-// 	char *mem = ft_strdup(pa->number);
-// 	if (!(five = ft_math_longar_str_exponent_mix("5", ex)))
-// 		return (NULL);
-// 	ls = lst->next;
-// 	while (ls)
-// 	{
-// 		pa = ls->content;
-// 		pa->up = pa->number;
-// 		pa->number = ft_math_longar_str_multi_free_1(ft_math_longar_str_exponent_mix("2", index), pa->number);
-// 		ls = ls->next;
-// 		index++;
-// 	}
-// 	l = lst;
-// 	while (l)
-// 	{
-// 		pa = l->content;
-// 		l = l->next;
-// 		result = ft_math_longar_str_add_free_1(result, pa->up);
-// 	}
-// 	result = ft_math_longar_str_multi_free_1(result, five);
-// 	result = ft_str_fix_width_of_line_free(result, '0', ft_strlen(ft_math_longar_str_multi(mem, five)) - 1, "alignment: left, crop: no");
-// 	return (result);
-// }
 
-char *gg(t_list *lst)
+char *two_five(size_t two)
 {
-	t_list *temp;
 	char *result;
-	char *temp_res;
-	size_t index = 0;
+	char *t;
+	char *f;
+	size_t size;
+
+	t = ft_math_longar_str_exponent_mix("2", (unsigned long int)two);
+	f = ft_math_longar_str_exponent_mix("5", (unsigned long int)two);
+	t = ft_math_longar_str_multi_free_1(t, f);
+	if (!(size = ft_strlen_safe(t)))
+		return (ft_free_both(t, f));
+	if (!(two = ft_strlen_safe(f)))
+		return (ft_free_both(t, f));
+	size--;
+	if (two != size)
+	{
+		result = ft_strjoin_fr_both(ft_strnew_filler(size - two, '0'), f);
+		return (result);
+	}
+	return (f);
+}
+
+char *itoa_adder(t_list *lst)
+{
+	char *result;
+	t_list *temp;
+	char *res_2;
+
 	temp = lst;
-	result = NULL;
-	ft_lst_putstr_sep(lst, ", ");
-	ft_putchar('\n');
+	if (!(result = ft_strdup("0")))
+	{
+		ft_lst_free_chain(lst);
+		return (NULL);
+	}
 	while (lst)
 	{
-		if (lst->next)
+		if (lst->content)
 		{
-			result = ft_math_longar_str_add(lst->content, (lst->next)->content);
+			result = ft_math_longar_str_add_free_1(result, lst->content);
 			result[0] = '1';
 		}
-		index++;
 		lst = lst->next;
 	}
-	if (!index)
-		result = ft_strdup(lst->content);
-	ft_lst_free_chain(temp);
-	temp_res = ft_strdup(ft_jump(result, 1));
+	res_2 = ft_strdup(ft_jump(result, 1));
 	free(result);
-	ft_putstr("ahah\n");
-	return (temp_res);
+	return (res_2);
 }
 
-char *yy(t_list *lst, size_t size)
+size_t max_sizes(t_list *lst)
 {
-	t_list *temp;
+	size_t result;
 
-	temp = lst;
-	ft_lst_putstr_sep(lst, ", ");
-	ft_putchar('\n');
+	result = 0;
 	while (lst)
 	{
-		// if (ft_strlen(lst->content) != size)
-		// {
-		// 	lst->content = ft_strjoin_fr_both(ft_strnew_filler(size - ft_strlen(lst->content), '0'), lst->content);
-		// }
-		lst->content = ft_str_fix_width_of_line_free(lst->content, '0', size, "alignment: right, crop: no");
-		lst->content = ft_strjoin_free_2("1", lst->content);
+		if (lst->content_size > result)
+			result = lst->content_size;
 		lst = lst->next;
 	}
-	ft_putstr("ohoh\n");
-	return (gg(temp));
+	return (result);
 }
 
-char *f(t_list *lst)
+char *itoa_alignment(t_list *lst)
 {
-	char *five;
-	size_t len;
 	t_list *temp;
+	size_t max_size;
+	size_t this_size;
 
 	if (!lst)
 		return (ft_strdup("0"));
-	if (!(five = ft_math_longar_str_exponent_mix("2", lst->content_size + 1)))
-	{
-		ft_lst_free_chain(lst);
-		return (NULL);
-	}
-	if (!(five = ft_math_longar_str_multi_free_1(five, lst->content)))
-	{
-		ft_lst_free_chain(lst);
-		return (NULL);
-	}
 	temp = lst;
-	len = ft_strlen(five) - 1;
-	ft_putstr("vot = ");
-	ft_putnbr((int)len);
-	ft_putchar('\n');
-	free(five);
-	ft_lst_putstr_sep(lst, ", ");
-	ft_putchar('\n');
+	max_size = max_sizes(lst);
 	while (lst)
 	{
-		if (!(five = ft_math_longar_str_exponent_mix("5", lst->content_size - 1)))
+		if (lst->content)
 		{
-			ft_lst_free_chain(temp);
-			return (NULL);
+			this_size = ft_strlen(lst->content);
+			if (this_size != max_size)
+			{
+				lst->content = ft_strjoin_fr_both(lst->content, \
+					ft_strnew_filler(max_size - this_size, '0'));
+				lst->content = ft_strjoin_free_2("1", lst->content);
+			}
 		}
-		lst->content = ft_math_longar_str_multi_free_both(lst->content, five);
 		lst = lst->next;
 	}
-	ft_putstr("haha\n");
-	return (yy(temp, len));
+	return (itoa_adder(temp));
 }
 
 char *float_part(unsigned long int mantisa, unsigned int num_bits)
@@ -189,40 +141,107 @@ char *float_part(unsigned long int mantisa, unsigned int num_bits)
 	index = num_bits;
 	lst = NULL;
 	kk = 1;
-
 	ft_str_reverse(listing);
-	ft_putstr(&listing[index]);
-		ft_putstr("\n");
 	while (listing[index])
 	{
 		if (listing[index] == '1')
-			ft_lstadd(&lst, ft_lstnew_without_copy(ft_math_longar_str_exponent_mix("5", (unsigned long int)kk), kk + 1 ));
+			ft_lstadd(&lst, ft_lstnew_without_copy(two_five(kk), kk + 1));
 		kk++;
 		index++;
 	}
 	free(listing);
-	ft_putstr("hehe\n");
-	return (f(lst));
+	return (itoa_alignment(lst));
 }
 
-char *res(long double num)
+int				one_num_from_multi(char *num, size_t *ind_in)
+{
+	size_t				ind_out;
+
+	if (!*(num + 1))
+	{
+		printf("if 1, if 1, sign: %c\n", *num);
+		*ind_in = 0;
+		return ((int)(num - '0'));
+	}
+	if (one_num_from_multi(num + 1, &ind_out) + ind_out >= 5)
+	{
+		*ind_in = 1;
+		printf("if 2, sign: %c, ind: %lu\n", *num, ind_out);
+		return (0);
+		// if (*num - '0' + ind_out <= 9)
+		// {
+		// 	printf("if 2, if 1, sign: %c, ind: %lu\n", *num, ind_out);
+		// 	printf("%lu\n", *num - '0' + ind_out);
+		// 	*ind_in = 0;
+		// 	return (0);
+		// }
+		// else
+		// {
+		// 	printf("if 2, if 2, sign: %c\n", *num);
+		// 	*ind_in = 1;
+		// 	return ((*num - '0' + ind_out) - 10);
+		// }
+	}
+	*ind_in = 0;
+	printf("return, sign: %c\n", *num);
+	return ((int)*num - '0' + ind_out);
+}
+
+char *ft_round(char *num, size_t accuracy)
+{
+	char *temp;
+	size_t i;
+	size_t size;
+
+	temp = ft_strchr(num, '.') + 1;
+	ft_putstr(temp);
+	ft_putstr("\n");
+	temp[accuracy - 1] = (char)one_num_from_multi(num + accuracy - 1, &i) + i + '0';
+	temp[accuracy] = '\0';
+	if (i)
+	{
+		printf("jj\n");
+		while (accuracy)
+		{
+			if (temp[accuracy - 1] - '0' + i > 9)
+				temp[accuracy - 1] = '0';
+			else
+			{
+				temp[accuracy - 1] = temp[accuracy - 1] + 1;
+				i = 0;
+				break ;
+			}
+			accuracy--;
+		}
+		if (i)
+			return (ft_strdup_n_free(num, ft_strlen(num) - (temp - num)));
+	}
+	return (ft_strdup_free(num));
+}
+
+char *ft_itoa_specific(long double num, size_t accuracy)
 {
 	t_double fl;
+	char *result;
 
 	fl.d = num;
 	fl.s_parts.exponent -= 16382;
-	unsigned long int j = fl.s_parts.mantisa;
-	char *result = ft_strjoin(integer_part(fl.s_parts.mantisa, fl.s_parts.exponent), float_part(fl.s_parts.mantisa, fl.s_parts.exponent));
-	ft_putstr("gogo\n");
+	if (!(result = ft_strjoin_fr_both(integer_part(fl.s_parts.mantisa, \
+		fl.s_parts.exponent), float_part(fl.s_parts.mantisa, \
+			fl.s_parts.exponent))))
+		return (NULL);
 	if (fl.s_parts.sign)
 		result = ft_strjoin_free_2("-", result);
+	result = ft_round(result, accuracy);
 	return (result);
 }
 
 int	main()
 {
 	//printf("%s\n", res(1.875 ));
-	printf("%s\n", res(1.9375));
-	printf("%f\n", 1.9375);
+	printf("%s\n", ft_itoa_specific(1.65, 6));
+	//printf("%s\n", ft_itoa_long_double_old(1.65, 6));
+
+	printf("%f\n", 1.66);
 	return (0);
 }
